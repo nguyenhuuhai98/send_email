8e4eea50cf6c092684ab8c34e497fcb0a847e7e1
hainh-0963 <nguyen.huu.hai@sun-asterisk.com>
2020-04-15 16:57:48 +0700

    First commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendEmail;
+use App\Task;
+use App\User;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return view('tasks', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $task = new Task();
+        $task->name = $request->name;
+        $task->save();
+
+        $users = User::all();
+        $message = [
+            'type' => 'Create task',
+            'task' => $task->name,
+            'content' => 'has been created!',
+        ];
+        SendEmail::dispatch($message, $users)->delay(now()->addMinute(1));
+
+        return redirect()->back();
+    }
+
+    public function delete($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        $users = User::all();
+        $message = [
+            'type' => 'Delete task',
+            'task' => $task->name,
+            'content' => 'has been deleted!',
+        ];
+        SendEmail::dispatch($message, $users)->delay(now()->addMinute(1));
+
+        return redirect()->back();
+    }
+}
